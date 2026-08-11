89d400f0670001ecb3ff2254eedd1aa61952bd74
grverlearner <grverlearner@gmail.com>
2026-08-10 21:25:12 -0500

    actualizacion icon

--- resources/views/layouts/hub.blade.php
+++ resources/views/layouts/hub.blade.php
@@ -10,15 +10,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="theme-color" content="#020617">
-
+    <link rel="icon" type="image/png" href="{{ asset('images/joganboruto.jpg') }}">
     <title>
         @yield('title', 'Centro OmniMerge') | OmniMerge
     </title>
-
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet">
 
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
